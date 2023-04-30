a9dba1547d3a9a360419fb98548d6dec071c54fb
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-04-30 21:27:23 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\CareerDevelopmentApplication;
+use App\Models\JobInterview;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
             JobPostSeeder::class,
             CareerDevelopmentSeeder::class,
             CareerDevelopmentApplicationSeeder::class,
+            JobApplicationSeeder::class,
+            JobInterviewSeeder::class,
         ]);
     }
 }
