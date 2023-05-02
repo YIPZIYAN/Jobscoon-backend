e915f14333163a828b290b96a1aba4d55dbc7210
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-05-02 18:37:29 +0800

    Update JobPostFactory.php

--- database/factories/JobPostFactory.php
+++ database/factories/JobPostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class JobPostFactory extends Factory
 {
+
+    private const TYPE = [
+        'Full-time',
+        'Part-time',
+    ];
+
+    private const SHIFT = [
+        'Monday to Thursday',
+        'Monday to Friday',
+        'Monday to Saturday',
+        'Saturday and Sunday',
+        'Friday to Sunday',
+    ];
     /**
      * Define the model's default state.
      *
@@ -16,8 +30,22 @@ class JobPostFactory extends Factory
      */
     public function definition(): array
     {
+        $salaryLower = fake()->numberBetween(1500,9999);
+        $salaryHigher = fake()->numberBetween(1500,9999);
+        $type = fake()->randomElement(SELF::TYPE);
+        $shift = fake()->randomElement(SELF::SHIFT);
+        do{
+            $salaryHigher = fake()->numberBetween(1500,9999);
+        }while($salaryLower > $salaryHigher);
         return [
-            //
+            'title' => fake()->jobTitle(),
+            'salary_lower' => $salaryLower,
+            'salary_upper' => $salaryHigher,
+            'type' => $type,
+            'shift' => $shift,
+            'description' => fake()->paragraph(),
+            'company_id' => Company::all()->id->random(),
+            
         ];
     }
 }
